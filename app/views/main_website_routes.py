626555fcbd5107d65f975957da5a4a178de5a968
marielle0604 <modestomarielle127@gmail.com>
2026-05-25 20:15:26 +0800

    create a landing page for about us

--- app/views/main_website_routes.py
+++ app/views/main_website_routes.py
@@ -5,9 +5,14 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def landingpage():
     current_plan = session.get('plan', 'free')
+    return render_template('landing.html', current_plan=current_plan)
+
+@main.route('/main')
+def plans():
+    current_plan = session.get('plan', 'free')
     return render_template('main.html', current_plan=current_plan)
 
 @main.route('/plans')
-def plans():
+def plans_and_credits():
     current_plan = session.get('plan', 'free')
     return render_template('plans_and_credits.html', current_plan=current_plan)
