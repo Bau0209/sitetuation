f815b75380b8a6c66ba5bc17bf0c1fe38e32ccd7
marielle0604 <modestomarielle127@gmail.com>
2026-05-25 13:23:22 +0800

    build the frontend

--- app/views/main_website_routes.py
+++ app/views/main_website_routes.py
@@ -1,7 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def landingpage():
-    return render_template('/main.html')
+    current_plan = session.get('plan', 'free')
+    return render_template('main.html', current_plan=current_plan)
+
+@main.route('/plans')
+def plans():
+    current_plan = session.get('plan', 'free')
+    return render_template('plans_and_credits.html', current_plan=current_plan)
